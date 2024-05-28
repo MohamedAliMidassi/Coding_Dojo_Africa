a88facfe93191db67e114022c2b78dd00bfaf04a
MohamedAliMidassi <mohamedalimidassi@gmail.com>
2024-05-28 16:04:14 +0100

    ;)

---
+++ mern/Week 1/Day 4/Core/First_JSX/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1>
+      Hello Dojo!
+    </h1>
+    <h2>
+      Things I need  
+    </h2>
+    <ul>
+      <li>
+        learn React
+      </li>
+      <li>
+        Climb Mt.Everest
+      </li>
+      <li>
+        Run a marathon
+      </li>
+      <li>
+        Feed the dogs
+      </li>
+    </ul>
+    </>
+  );
+}
+
+export default App
