ad18926532d1eded0464a6b964c2ed4f8080b5b2
MohamedAliMidassi <mohamedalimidassi@gmail.com>
2024-06-01 13:47:27 +0100

    ;)

--- mern/Week 1/Day 5/Core/More_Form/src/components/User.jsx
+++ mern/Week 1/Day 5/Core/More_Form/src/components/User.jsx
@@ -29,22 +29,22 @@ const User = () => {
             <div className='row' >
                 <label className="form-label">First Name </label>
                 <input type="text"className="form-control" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-                {firstName.length >= 0 & firstName.length < 2 ? <p style={{ color: "red", textAlign: 'left' }}>First Name has to be more than two characters</p> : <p></p>}
+                {firstName.length >= 1 & firstName.length < 2 ? <p style={{ color: "red", textAlign: 'left' }}>First Name has to be more than two characters</p> : <p></p>}
             </div>
             <div className='row'>
                 <label className="form-label">Last Name </label>
                 <input type="text"className="form-control" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-                {lastName.length >= 0 & lastName.length < 2 ? <p style={{ color: "red", textAlign: 'left' }}>Last Name has to be more than two characters</p> : <p></p>}
+                {lastName.length >= 1 & lastName.length < 2 ? <p style={{ color: "red", textAlign: 'left' }}>Last Name has to be more than two characters</p> : <p></p>}
             </div>
             <div className='row'>
                 <label className="form-label">Email Address </label>
                 <input type="text"className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} />
-                {email.length >= 0 & email.length < 2 ? <p style={{ color: "red", textAlign: 'left' }}>Email address has to be more than two characters</p> : <p></p>}
+                {email.length >= 1 & email.length < 2 ? <p style={{ color: "red", textAlign: 'left' }}>Email address has to be more than two characters</p> : <p></p>}
             </div>
             <div className='row'>
                 <label className="form-label">Password </label>
                 <input type="password"className="form-control" value={password} onChange={(e) => setPassword(e.target.value)} />
-                {password.length >= 0 & password.length <= 8 ? <p style={{ color: "red", textAlign: 'left' }}>Password has to be more than 8 characters</p> : <p></p>}
+                {password.length >= 1 & password.length <= 8 ? <p style={{ color: "red", textAlign: 'left' }}>Password has to be more than 8 characters</p> : <p></p>}
             </div>
             <div className='row'>
                 <label className="form-label">Confirm Password </label>
