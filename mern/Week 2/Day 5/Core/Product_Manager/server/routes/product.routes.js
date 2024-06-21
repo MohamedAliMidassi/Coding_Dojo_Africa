f03e44bf96560abd0908bb784b2a53154b9f9fd4
MohamedAliMidassi <156659260+MohamedAliMidassi@users.noreply.github.com>
2024-06-21 14:16:50 +0100

    Delete mern/Week 2/Day 5/Core/Product_Manager directory

--- mern/Week 2/Day 5/Core/Product_Manager/server/routes/product.routes.js
+++
@@ -1,9 +0,0 @@
-const ProductController = require('../controllers/product.controller');
-
-module.exports = app => {
-    app.get('/api', ProductController.findAll);
-    app.get('/api/product/:id', ProductController.getOneProduct);
-    app.post('/api/product', ProductController.createProduct);
-
-}
-
